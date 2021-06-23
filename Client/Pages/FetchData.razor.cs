6b51bdcf4cb7fdc5bb1f363930833016ddc48a6c
mattruma <mattruma@gmail.com>
2021-06-23 14:27:29 -0400

    initial commit

---
+++ Client/Pages/FetchData.razor.cs
@@ -0,0 +1,31 @@
+﻿using BlazorApp.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Client.Pages
+{
+    [Authorize]
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts;
+
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("/api/WeatherForecast");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
